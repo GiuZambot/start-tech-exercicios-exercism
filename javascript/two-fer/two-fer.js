6ab11fa17852e9ea573addde3370c96f25f86cf2
Giu Zambot <giuzambot@gmail.com>
2021-08-18 17:14:31 -0300

    Arrumei mais bonitinho

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const twoFer = (name) => {
     name = name || "you";
     return `One for ${name}, one for me.`;
