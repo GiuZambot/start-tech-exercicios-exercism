9c17116fdfab32950096753746a2b0b35e91e2eb
Giu Zambot <giuzambot@gmail.com>
2021-08-19 15:08:57 -0300

    Simplificação

--- teams/start-tech/javascript/gigasecond/gigasecond.js
+++ teams/start-tech/javascript/gigasecond/gigasecond.js
@@ -1,4 +1,4 @@
 export const gigasecond = (date) => {
-    const futureDate = new Date(Date.parse(date)).getTime() + 10 ** 12; // Data recebida mais um gigasegundo.
+    const futureDate = date.getTime() + 1e12; // Data recebida mais um gigasegundo.
     return new Date(futureDate);
 };
