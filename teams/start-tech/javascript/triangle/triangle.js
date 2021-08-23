53cc8df9ab0375d43b45ccaf3d9ba6db86e875f3
Giu Zambot <giuzambot@gmail.com>
2021-08-23 10:12:40 -0300

    Versão com validação separada

--- teams/start-tech/javascript/triangle/triangle.js
+++ teams/start-tech/javascript/triangle/triangle.js
@@ -1,24 +1,19 @@
+// Segunda versão com verificação pelo método isDegenerate
 export class Triangle {
     constructor(...sides) {
         // Lados do Triangulo para construção do triangulo
         this.ladoA = sides[0];
         this.ladoB = sides[1];
         this.ladoC = sides[2];
+        this.sides = sides;
 
         // Determina quantos valores únicos tem o triangulos (inverso do número de lados iguais)
         this.ladosUnicosDoTriangulo = new Set(sides).size;
-
-        // Verifica se o triangulo é possível segundos as regras trigonométricas
-        const trianguloComZero = sides.some(x => x === 0);
-        const regraExistenciaUm = this.ladoA < (this.ladoB + this.ladoC);
-        const regraExistenciaDois = this.ladoB < (this.ladoA + this.ladoC);
-        const regraExistenciaTres = this.ladoC < (this.ladoA + this.ladoB);
-        this.trianguloPodeExistir = regraExistenciaUm && regraExistenciaDois && regraExistenciaTres && !trianguloComZero;
     }
 
     get isEquilateral() {
         // Resultar falso se não puder existir
-        if (!this.trianguloPodeExistir) return false;
+        if (this.isDegenerate) return false;
 
         // Não tem todos lados iguais (um valor único)
         if (this.ladosUnicosDoTriangulo !== 1) return false;
@@ -29,7 +24,7 @@ export class Triangle {
 
     get isIsosceles() {
         // Resultar falso se não puder existir
-        if (!this.trianguloPodeExistir) return false;
+        if (this.isDegenerate) return false;
 
         // Não tem dois lados iguais (dois valores únicos)
         if (this.ladosUnicosDoTriangulo === 3) return false;
@@ -40,7 +35,7 @@ export class Triangle {
 
     get isScalene() {
         // Resultar falso se não puder existir
-        if (!this.trianguloPodeExistir) return false;
+        if (this.isDegenerate) return false;
 
         // Não tem todos lados diferentes (três valores únicos)
         if (this.ladosUnicosDoTriangulo !== 3) return false;
@@ -49,9 +44,12 @@ export class Triangle {
         return true;
     }
 
-
     get isDegenerate() {
-        // extra para desafios de triangulos degenerados
-        return !this.trianguloPodeExistir;
+        // Verifica triangulos degenerados pelas regras de existência dos triangulos
+        const trianguloComZero = this.sides.some(x => x === 0);
+        const regraExistenciaUm = this.ladoA >= (this.ladoB + this.ladoC);
+        const regraExistenciaDois = this.ladoB >= (this.ladoA + this.ladoC);
+        const regraExistenciaTres = this.ladoC >= (this.ladoA + this.ladoB);
+        return regraExistenciaUm || regraExistenciaDois || regraExistenciaTres || trianguloComZero;
     }
 }
