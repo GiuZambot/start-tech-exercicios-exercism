70f77cb42af74d2bf3dc42bd34323f64a0eea00b
Giu Zambot <giuzambot@gmail.com>
2021-08-25 09:29:32 -0300

    Hamming diferenças no na sequência +line

--- teams/start-tech/javascript/hamming/hamming.js
+++ teams/start-tech/javascript/hamming/hamming.js
@@ -1,12 +1,17 @@
 export const compute = (left, right) => {
     if (!left && !right) return 0;
+
     if (left && !right)
         throw new Error('right strand must not be empty');
+
     if (!left && right)
         throw new Error('left strand must not be empty');
+
     if (left.length !== right.length)
         throw new Error('left and right strands must be of equal length');
+
     if (left === right) return 0;
+
     if (left !== right) {
         let diferentes = 0;
         for (let i = 0; i < left.length; i++) {
