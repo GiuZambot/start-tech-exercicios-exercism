639b4811c0615bdfc8c25083a55ee736919f505c
Giu Zambot <giuzambot@gmail.com>
2021-08-25 08:46:26 -0300

    Outra forma de fazer

--- teams/start-tech/javascript/protein-translation/protein-translation.js
+++ teams/start-tech/javascript/protein-translation/protein-translation.js
@@ -1,37 +1,46 @@
+// Versão com isStop no objeto
 export const translate = (rna) => {
     // Rna vazio retona uma array vazia  
     if (!rna) return [];
 
     // Divide o RNA em Codons, divide a String em grupos de 3 caracteres
-    const codons = rna.split(/(\w{3})/).filter(x => x);
+
+    // Com slipt e regex
+    //const codons = rna.split(/(\w{3})/).filter(x => x);
+
+    // Com for simples
+    const codons = [];
+    for (let i = 0; i < rna.length; i += 3) {
+        codons.push(rna[i] + rna[i + 1] + rna[i + 2]);
+    }
 
     // Converte os codons em nomes de proteínas
     const protein = [];
     for (let codon = 0; codon < codons.length; codon++) {
-        if (mapaCP[codons[codon]] === 'STOP') break;
-        protein.push(mapaCP[codons[codon]]);
         if (mapaCP[codons[codon]] === undefined) throw new Error('Invalid codon');
+        if (mapaCP[codons[codon]].isStop) break;
+        protein.push(mapaCP[codons[codon]].name);
     }
 
     return protein;
 };
 
 const mapaCP = {
-    AUG: 'Methionine',
-    UUU: 'Phenylalanine',
-    UUC: 'Phenylalanine',
-    UUA: 'Leucine',
-    UUG: 'Leucine',
-    UCU: 'Serine',
-    UCC: 'Serine',
-    UCA: 'Serine',
-    UCG: 'Serine',
-    UAU: 'Tyrosine',
-    UAC: 'Tyrosine',
-    UGU: 'Cysteine',
-    UGC: 'Cysteine',
-    UGG: 'Tryptophan',
-    UAA: 'STOP',
-    UAG: 'STOP',
-    UGA: 'STOP'
+    AUG: { isStop: false, name: "Methionine" },
+    UUU: { isStop: false, name: "Phenylalanine" },
+    UUC: { isStop: false, name: "Phenylalanine" },
+    UUA: { isStop: false, name: "Leucine" },
+    UUG: { isStop: false, name: "Leucine" },
+    UCU: { isStop: false, name: "Serine" },
+    UCC: { isStop: false, name: "Serine" },
+    UCA: { isStop: false, name: "Serine" },
+    UCG: { isStop: false, name: "Serine" },
+    UAU: { isStop: false, name: "Tyrosine" },
+    UAC: { isStop: false, name: "Tyrosine" },
+    UGU: { isStop: false, name: "Cysteine" },
+    UGC: { isStop: false, name: "Cysteine" },
+    UGG: { isStop: false, name: "Tryptophan" },
+    UAA: { isStop: true, name: "UAA" },
+    UAG: { isStop: true, name: "UAG" },
+    UGA: { isStop: true, name: "UGA" }
 };
