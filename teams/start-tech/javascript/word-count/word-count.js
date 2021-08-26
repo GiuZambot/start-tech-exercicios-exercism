80e7080d55025e5105e17b7cdfcb110cb3fc9931
Giu Zambot <giuzambot@gmail.com>
2021-08-26 14:05:57 -0300

    Sem o comentário esqueleto

--- teams/start-tech/javascript/word-count/word-count.js
+++ teams/start-tech/javascript/word-count/word-count.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Word Count' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const countWords = (frase) => {
     const palavrasArr = frase.
     toLowerCase().
