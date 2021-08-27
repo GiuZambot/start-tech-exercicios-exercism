34cb6b68068fc28eb50c5ac094fba98a82651d9f
Giu Zambot <giuzambot@gmail.com>
2021-08-27 13:47:58 -0300

    Grains 2.0

--- teams/start-tech/javascript/grains/grains.js
+++ teams/start-tech/javascript/grains/grains.js
@@ -6,9 +6,6 @@
  */
 
 //
-// This is only a SKELETON file for the 'Grains' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 export const square = (qtdade) => {
     if (qtdade < 1 || qtdade > 64) {
         throw new Error("square must be between 1 and 64");
@@ -17,17 +14,7 @@ export const square = (qtdade) => {
 };
 
 export const total = () => {
-    // Por for
-    // let somaDeGraos = BigInt(0);
-    // for (let n = 0; n < 64; n++) {
-    //     somaDeGraos += BigInt(2 ** n);
-    // }
-
     // Por matemática
-    let somaDeGraos = BigInt(2 * (2 ** 63)) - BigInt(1);
-
-    // Dá erro, não subtrai o último 1
-    //let somaDeGraos = BigInt((2 * (2 ** 63)) - (1));
-
+    let somaDeGraos = BigInt(2 * (2 ** 63)) - 1n;
     return somaDeGraos;
 };
