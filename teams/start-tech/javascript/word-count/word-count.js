cd098feab0db0f324fd275b81f35287fb9c93b5f
Giu Zambot <giuzambot@gmail.com>
2021-08-27 09:50:07 -0300

    Word Count 2

--- teams/start-tech/javascript/word-count/word-count.js
+++ teams/start-tech/javascript/word-count/word-count.js
@@ -1,13 +1,8 @@
 export const countWords = (frase) => {
     const palavrasArr = frase.
     toLowerCase().
-    replace(/\b'\b/g, 'A').
-    replace('\n', '').
-    split(/\W+/g).
-    filter(x => x).
-    join(" ").
-    replace(/A/g, "'").
-    split(" ");
+    split(/\s|'\B|\B'|\\n|[.,:!!&@$%^&]/g).
+    filter(x => x);
 
     const wordCount = {};
     palavrasArr.map(x => wordCount[x] = (x in wordCount) ? wordCount[x] + 1 : 1);
