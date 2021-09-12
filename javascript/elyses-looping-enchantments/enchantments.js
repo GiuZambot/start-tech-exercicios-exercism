ec357d663e42025423e153ece0984ab12c8e7d07
Giu Zambot <giuzambot@gmail.com>
2021-09-12 16:09:07 -0300

    Emchantments Looping

---
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -0,0 +1,28 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+  const wordCount = {};
+  stack.map(x => wordCount[x] = (x in wordCount) ? wordCount[x] + 1 : 1);
+  return wordCount[card] ?? 0;
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  const wordCount = { true: 0, false: 0 };
+  stack.forEach(x => wordCount[!(x % 2)]++);
+  return wordCount[type];
+}
