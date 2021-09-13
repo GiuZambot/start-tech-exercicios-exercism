f3f7eeb4a0741e1bad48ba56f1d6e28fdcbb10a2
Giu Zambot <giuzambot@gmail.com>
2021-09-13 07:50:57 -0300

    Delete teams directory

--- teams/start-tech/javascript/pangram/pangram.js
+++
@@ -1,7 +0,0 @@
-export const isPangram = (frase) => {
-    if (!frase) return false;
-
-    const fraseLowerCase = frase.toLowerCase();
-    const fraseSet = new Set(fraseLowerCase.replace(/[^a-z]/g, "").split('')).size;
-    return fraseSet === 26;
-};
