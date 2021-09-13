f2974186af5efac83ef31d50691c08be22972fc4
Giu Zambot <giuzambot@gmail.com>
2021-09-13 07:23:51 -0300

    Elyses Analytic Enchantment

--- README.md
+++
@@ -1,1 +0,0 @@
-# Exercícios do site Exercism
--- javascript/transferir.bat
+++
@@ -1,1 +0,0 @@
-robocopy . C:\Users\giuliano.furlan\Exercism\teams\start-tech\javascript /e /purge /xd node_modules /xd .git /xd .vscode /xf .gitignore
