050369b00783bbdf4d841ca528d93fdb5afcb4d8
Giu Zambot <giuzambot@gmail.com>
2021-10-04 18:45:11 -0300

    Resistor Color TS

---
+++ typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,4 @@
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+export const colorCode = (color: string): number => {
+  return COLORS.findIndex(c => c === color);
+}
