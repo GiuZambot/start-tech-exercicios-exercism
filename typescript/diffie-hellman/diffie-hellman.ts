7193c13eb138cd952ee9cf99f5791a21ae011c89
Giu Zambot <giuzambot@gmail.com>
2021-10-05 15:34:56 -0300

    Diffie-Hellman

--- typescript/diffie-hellman/diffie-hellman.ts
+++ typescript/diffie-hellman/diffie-hellman.ts
@@ -3,16 +3,16 @@ export class DiffieHellman {
   private readonly _g: number;
 
   constructor(p: number, g: number) {
-    if (!p || p > 100 || !this.ehNumPrimo(p))
+    if (!p || p > 50 || !this.ehNumPrimo(p))
       throw 'Primeiro número não é Primo';
-    if (!g || p > 100 || !this.ehNumPrimo(g))
+    if (!g || p > 50 || !this.ehNumPrimo(g))
       throw 'Segundo número não é Primo';
     this._p = p;
     this._g = g;
   }
 
   private ehNumPrimo(numeroATestar: number): boolean {
-    for (let i = 2; i < numeroATestar; i++)
+    for (let i = 2; i < Math.floor(numeroATestar ** 0.5); i+=2)
       if (numeroATestar % i === 0) return false;
     return numeroATestar > 1;
   }
